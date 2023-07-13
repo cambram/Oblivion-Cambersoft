932509cc7530ebbb96a7351b8ed74847cd612ca9
cambram <91072006+cambram@users.noreply.github.com>
2023-07-13 18:20:53 +0200

    Main Menu Fade In and Out

--- Assets/Scripts/Main_Menu/MainMenu.cs
+++ Assets/Scripts/Main_Menu/MainMenu.cs
@@ -5,11 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField]
+    private Animator animator;
+
     public void LoadGame() {
-        SceneManager.LoadScene(1);
+        animator.SetTrigger("FadeOut");
+        StartCoroutine(SceneChangeRoutine());
     }
 
     public void QuitGame() {
         Application.Quit();
+    }
+    IEnumerator SceneChangeRoutine() {
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(1);
     }
 }
