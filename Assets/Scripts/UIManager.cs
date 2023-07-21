5ebc5f7c5935bf8494bd363874f457aa7cf5261f
cambram <91072006+cambram@users.noreply.github.com>
2023-07-21 18:38:12 +0200

    Adding the tutorial icons

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -9,17 +9,11 @@ public class UIManager : MonoBehaviour
     private Text _deathText;
     private bool _isDead;
 
-    [SerializeField]
-    private Sprite [] _batterySprites;
-    [SerializeField]
-    private Image _batteryImage;
-
     private GameManager _gameManager;
 
     void Start()
     {
         _deathText.gameObject.SetActive(false);
-        _batteryImage.sprite = _batterySprites[4];
         _isDead = false;
         _gameManager = GameObject.Find("Game_Manager").GetComponent<GameManager>();
     }
@@ -34,12 +28,4 @@ public class UIManager : MonoBehaviour
         _deathText.gameObject.SetActive(true);
         _isDead = true;
     }
-
-    /// <summary>
-    /// Updates the battery UI element.
-    /// </summary>
-    /// <param name="percent">Index for battery sprite. 0 = 0%; 1 = 25%; 2 = 50%; 3 = 75%; 4 = 100%</param>
-    /*public void UpdateBattery(int percent) {
-        _batteryImage.sprite = _batterySprites[percent];
-    }*/
 }
