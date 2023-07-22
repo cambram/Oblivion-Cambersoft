f867f77139ca6a89e77dcb5da3911ac4eef6167c
cambram <91072006+cambram@users.noreply.github.com>
2023-07-22 18:10:36 +0200

    flashlight and movement instructions implemented

--- Assets/Scripts/TutorialManager.cs
+++ Assets/Scripts/TutorialManager.cs
@@ -4,29 +4,68 @@ using UnityEngine;
 
 public class TutorialManager : MonoBehaviour
 {
+    //A vars
     [SerializeField]
     private GameObject _A;
     [SerializeField]
+    private Animator _AAnim;
+    //D vars
+    [SerializeField]
     private GameObject _D;
     [SerializeField]
+    private Animator _DAnim;
+    //E vars
+    [SerializeField]
     private GameObject _E;
+    //space vars
     [SerializeField]
     private GameObject _space;
+    //Right Click vars
     [SerializeField]
     private GameObject _rightClick;
+    //Left Click vars
     [SerializeField]
     private GameObject _leftClick;
+    [SerializeField]
+    private Animator _leftClickAnim;
+
+    private bool _flashlightInstructionComplete = false, _movementInstructionComplete = false;
+
+    private Player _player;
 
     void Start() {
+        _player = GameObject.Find("Player").GetComponent<Player>();
         _A.SetActive(false);
         _E.SetActive(false);
         _D.SetActive(false);
         _space.SetActive(false);
         _rightClick.SetActive(false);
         _leftClick.SetActive(false);
+        StartCoroutine(FlashlightInstruction());
     }
 
     void Update() {
-        
+        if (Input.GetMouseButtonDown(0) && !_flashlightInstructionComplete) {
+            _flashlightInstructionComplete = true;
+            _leftClickAnim.SetTrigger("FadeOut");
+            StartCoroutine(MovementInstruction());
+        }
+
+        if(_player.transform.position.x > -58 && !_movementInstructionComplete) {
+            _movementInstructionComplete = true;
+            _AAnim.SetTrigger("FadeOut");
+            _DAnim.SetTrigger("FadeOut");
+        }
+    }
+    
+    IEnumerator FlashlightInstruction() {
+        yield return new WaitForSeconds(1);
+        _leftClick.SetActive(true);
+    }
+
+    IEnumerator MovementInstruction() {
+        yield return new WaitForSeconds(1f);
+        _A.SetActive(true);
+        _D.SetActive(true);
     }
 }
